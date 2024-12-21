72a957968577048d4a0e1f6024b977ffed23b973
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2024-12-21 14:17:09 +0700

    Pemisahan css editor , konfigurasi vite

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -10,7 +10,7 @@
     {{--    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">--}}
 
     <!-- Styles -->
-    @vite('resources/css/app.css')
+    @vite('resources/css/editor.css')
 </head>
 <body class="antialiased">
 
---
+++ vite.config.mjs
@@ -0,0 +1,21 @@
+import {defineConfig} from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    build: {
+        outDir: 'public/build',
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/ts/App.ts', 'resources/css/app.css', 'resources/js/LDE/Main.js',
+            ],
+            refresh: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': '/resources/ts',
+        },
+    },
+});
