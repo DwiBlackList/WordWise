1df9afc0052fc5c6acf217d8c948e8cee6680f57
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2025-01-17 18:09:40 +0700

    Seeder User Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Student;
 use App\Models\Teacher;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Teacher::factory()->count(5)->create();
-        Student::factory()->count(100)->create();
+        // Teacher::factory()->count(5)->create();
+        // Student::factory()->count(100)->create();
+
+        // Create admin users
+        User::factory()->create([
+            'name' => 'Lunar Interactive',
+            'email' => 'lunarinteractive@gmail.com',
+            'password' => Hash::make('lunarinteractivestartup'),
+            'role' => 'admins',
+        ]);
+        
     }
 
 }
