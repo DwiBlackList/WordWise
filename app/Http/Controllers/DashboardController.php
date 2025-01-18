ba3e7c8c0eada728cd4640ab7bf1c7379bf2f889
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2025-01-18 18:12:55 +0700

    Dashboard 0.1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -34,7 +34,11 @@ class DashboardController extends Controller
         $activity = app(DashboardController::class)->Activity()->getData();
         $currentKnowledge = app(DashboardController::class)->CurrentKnowledge()->getData();
 
+        // List Kelas Untuk menampilkan seluruh kelas yang ada pada user tersebut , digunakan untuk ngelink dropdown
         $ListClass = $this->ListClass();
+
+        // Kelas pertama yang diambil untuk menampilkan data pada dashboard
+        $FirstClass = $ListClass->getData()[0];
         // dd($ListClass);
 
         return view('dashboard', [
@@ -146,88 +150,4 @@ class DashboardController extends Controller
         ]);
     }
 
-    public function DataTable(){
-        return response()->json([
-            'data' => [
-                [
-                    "name" => "Tiger Nixon",
-                    "content1" => "System Architect",
-                    "content2" => "Edinburgh",
-                    "content3" => "61",
-                ],
-                [
-                    "name" => "Garrett Winters",
-                    "content1" => "Accountant",
-                    "content2" => "Tokyo",
-                    "content3" => "63",
-                ],
-                [
-                    "name" => "Ashton Cox",
-                    "content1" => "Junior Technical Author",
-                    "content2" => "San Francisco",
-                    "content3" => "66",
-                ],
-                [
-                    "name" => "Cedric Kelly",
-                    "content1" => "Senior Javascript Developer",
-                    "content2" => "Edinburgh",
-                    "content3" => "22",
-                ],
-                [
-                    "name" => "Airi Satou",
-                    "content1" => "Accountant",
-                    "content2" => "Tokyo",
-                    "content3" => "33",
-                ],
-                [
-                    "name" => "Brielle Williamson",
-                    "content1" => "Integration Specialist",
-                    "content2" => "New York",
-                    "content3" => "61",
-                ],
-                [
-                    "name" => "Herrod Chandler",
-                    "content1" => "Sales Assistant",
-                    "content2" => "San Francisco",
-                    "content3" => "59",
-                ],
-                [
-                    "name" => "Rhona Davidson",
-                    "content1" => "Integration Specialist",
-                    "content2" => "Tokyo",
-                    "content3" => "55",
-                ],
-                [
-                    "name" => "Colleen Hurst",
-                    "content1" => "Javascript Developer",
-                    "content2" => "San Francisco",
-                    "content3" => "39",
-                ],
-                [
-                    "name" => "Sonya Frost",
-                    "content1" => "Software Engineer",
-                    "content2" => "Edinburgh",
-                    "content3" => "23",
-                ],
-                [
-                    "name" => "Jena Gaines",
-                    "content1" => "Office Manager",
-                    "content2" => "London",
-                    "content3" => "30",
-                ],
-                [
-                    "name" => "Quinn Flynn",
-                    "content1" => "Support Lead",
-                    "content2" => "Edinburgh",
-                    "content3" => "22",
-                ],
-                [
-                    "name" => "Charde Marshall",
-                    "content1" => "Regional Director",
-                    "content2" => "San Francisco",
-                    "content3" => "36",
-                ],
-            ],
-        ]);
-    }
 }
