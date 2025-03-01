03ea923bc181da722b462e7fd735a7f510f872a7
Ichsanul Dwi Prayitno <58975208+DwiBlackList@users.noreply.github.com>
2025-03-01 16:24:50 +0700

    Update console.php
    adding every 15 minutes fresh database ?

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote');
+Schedule::command('migrate:fresh --force')->everyFifteenMinutes();
+Schedule::command('db:seed --force')->everyFifteenMinutes();
